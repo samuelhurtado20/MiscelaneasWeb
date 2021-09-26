9a38f4b4d809a2b393dea5fbfb9ff08dfbcecb46
samuelhurtado20 <samuelhurtado20@hotmail.com>
2021-09-25 22:06:45 -0500

    Miscelaneas web page - init

---
+++ Miscelánea/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Miscelánea.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
